b9aba1368ea13e6e14cb337bbd7e57a5dcb1fd30
Lucas Henrique Lopes Costa <lucas20586@gmail.com>
2020-12-20 16:53:08 -0300

    BiFunction

--- src/main/java/functionaliinterface/_Function.java
+++ src/main/java/functionaliinterface/_Function.java
@@ -1,44 +1,79 @@
 package functionaliinterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
 
-        System.out.println("Função que SOMA 1");
-        int increment = incrementByOne(0);
-        System.out.println(increment);
+        System.out.println("Função (Pega um Argumentos) que SOMA 1 ");
 
-        // são as mesmas coisas
+            int increment = incrementByOne(0);
+            System.out.println(increment);
 
-        int increment2 = incrementByOneFunction.apply(1);
-        System.out.println(increment2);
+            // são as mesmas coisas
+
+            int increment2 = incrementByOneFunction.apply(0);
+            System.out.println(increment2);
 
         System.out.println("");
-        System.out.println("Função que MULTIPLICA por 10");
-        // multiplicando o resultado por 10
-        int multiply = multiplyBy10FUnction.apply(increment2);
-        System.out.println(multiply);
+        System.out.println("Função (Pega um Argumentos) que MULTIPLICA por 10");
 
-        // são as mesmas coisas
+            int multiply = multiplyBy10(2);
+            System.out.println(multiply);
 
-        Function<Integer, Integer> addBy1AndThenMultiplyBy10 =
-                incrementByOneFunction.andThen(multiplyBy10FUnction);
-        System.out.println(addBy1AndThenMultiplyBy10.apply(2));
+            // são as mesmas coisas
+
+            int multiply2 = multiplyBy10Function.apply(2);
+            System.out.println(multiply2);
+
+        System.out.println("");
+        System.out.println("Função (Pega um Argumentos) que SOMA 1 e MULTIPLICA por 10");
+
+            Function<Integer, Integer> addBy1AndThenMultiplyBy10 =
+                    incrementByOneFunction.andThen(multiplyBy10Function);
+            System.out.println(addBy1AndThenMultiplyBy10.apply(2));
+
+        System.out.println("");
+        System.out.println("BiFunction (Pega dois Argumentos) que SOMA 1 e MULTIPLICA por 10");
+
+            System.out.println(
+                    incrementByOneAndMultiply(4, 100)
+            );
+
+            // são as mesmas coisas
+
+            System.out.println(
+                    incrementByOneAndMultiplyBiFunction.apply(4, 100)
+            );
     }
 
-    // função que soma mais 1
-    static Function<Integer, Integer> incrementByOneFunction =
-            number -> number + 1;
-
-        // são as mesmas coisas
+    // Função que Soma 1
         static int incrementByOne(int number) {
             return number + 1;
         }
+    
+        // são as mesmas coisas
 
-    // função que multiplica por 10
-    static Function<Integer, Integer> multiplyBy10FUnction =
-        number -> number * 10;
+        static Function<Integer, Integer> incrementByOneFunction =
+            number -> number + 1;
 
 
+    // Função que Multiplica por 10
+        static int multiplyBy10 (int number) {
+            return number * 10;
+        }
+    
+        static Function<Integer, Integer> multiplyBy10Function =
+            number -> number * 10;
+
+    // Função que Soma e Multiplica por 10 - Pegando 2 Parâmetros
+    static int incrementByOneAndMultiply(int number, int numToMultiply) {
+        return (number + 1) * numToMultiply;
+    }
+
+        // são as mesmas coisas
+        static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+                (numberToIncrementByOne, numberToMultiplyBy)
+                        -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
 }
