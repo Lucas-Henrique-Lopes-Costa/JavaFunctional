fb7e301f53dbca465ccb2087eb1955f8c53d7bdb
Lucas Henrique Lopes Costa <lucas20586@gmail.com>
2020-12-24 11:58:26 -0300

    Linux

---
+++ src/main/java/functionaliinterface/_Consumer.java
@@ -0,0 +1,14 @@
+package functionaliinterface;
+
+public class _Consumer {
+    public static void main(String[] args) {
+
+    }
+
+    static class Customer {
+        private final String custumerName;
+        private final String custumerPhoneNumber;
+
+        Customer(String)
+    }
+}
